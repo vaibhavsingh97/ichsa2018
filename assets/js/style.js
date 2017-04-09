a6882590fabdb26d49b038a1267e17d3d22f57b7
vaibhavsingh97 <vaibhav.singh.14cse@bml.edu.in>
2017-04-09 13:47:25 +0530

    Update

---
+++ assets/js/style.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    // Dropdown
+    // $('.dropdown-button').dropdown({
+    //     inDuration: 300,
+    //     outDuration: 225,
+    //     constrain_width: false, // Does not change width of dropdown to that of the activator
+    //     hover: false, // Activate on hover
+    //     gutter: 0, // Spacing from edge
+    //     belowOrigin: false // Displays dropdown below the button
+    // });
+    $(".dropdown-button").dropdown();
+});
