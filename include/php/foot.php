7c4c675d0ee3414c7575ac8066e7bc0bcd5e021e
Vaibhav Singh <vaibhav.singh.14cse@bml.edu.in>
2017-06-19 12:19:52 +0530

    infinite loop bug fix

--- include/php/foot.php
+++ include/php/foot.php
@@ -29,9 +29,6 @@
     </div>
   </div>
 </footer>
-<?php if(is_page('#index-page')){
-
-} ?>
 <!--Import jQuery before materialize.js-->
 <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
 <script type="text/javascript" src="assets/js/materialize.js"></script>
