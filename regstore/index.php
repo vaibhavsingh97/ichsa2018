644a707fa6a5a56e8871d38d19c24c2911543365
Vaibhav Singh <vaibhav.singh.14cse@bml.edu.in>
2017-12-30 21:32:29 +0530

    Added beautification to admin

--- regstore/index.php
+++ regstore/index.php
@@ -1,4 +1,24 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link type="text/css" rel="stylesheet" href="../assets/css/materialize.min.css" media="screen,projection" />
+    <title>Document</title>
+</head>
+<style>
+    td{
+        margin-left:20px;
+        float:left;
+    }
+</style>
+<body>
+    <div class="container">
+        <div class="row">
+        <div class="collection">
+        <?php
 function http_digest_parse($txt) {
     $needed_parts = array('nonce'=>1, 'nc'=>1, 'cnonce'=>1, 'qop'=>1, 'username'=>1, 'uri'=>1, 'response'=>1);
     $data = array();
@@ -31,19 +51,31 @@ $valid_response = md5($A1.':'.$data['nonce'].':'.$data['nc'].':'.$data['cnonce']
 
 if ($data['response'] != $valid_response)
     die('Wrong Credentials!');
-
-echo 'You are logged in as: '.$data['username']. "<br><br>";
+?>
+<li class="collection-header center"><h4><?php echo 'Loggined as:&nbsp;&nbsp;&nbsp;'.$data['username'] ?></h4></li>
+<?php
 
 if(isset($_GET['ref'])) {
     $g = $_GET['ref'];
     $f = file_get_contents("$g/data.json");
     $f = json_decode($f);
     foreach($f as $k => $s) {
-        echo "$k: $s<br>";
+        echo '<table><tbody><tr><td style="font-weight:bold;">'.$k.':</td><td style="padding-left:20px;">'.$s.'</td></tbody></table>';
     }
 } else {
     $dirs = array_filter(glob('*'), 'is_dir');
+?>
+<?php
     foreach($dirs as $k) {
-        echo "<a href='?ref=$k'>$k</a><br>";
+        echo "<a style='font-size:20px;' href='?ref=$k' class='collection-item'>$k</a>";
     }
+?>
+</div>
+<?php
 }
+?>
+        </div>
+    </div>
+</body>
+<script type="text/javascript" src="../assets/js/materialize.js"></script>
+</html>
